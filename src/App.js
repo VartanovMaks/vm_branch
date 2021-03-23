c322eeb9bf849d86b4bd829c50863212b86787cc
VartanovMaks <vartanov2009@gmail.com>
2021-03-23 10:02:52 +0200

    Приложение часы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function tick(){
+  const element = (
+      <div>
+        <h1>Hello, world</h1>
+        <h2> It is {new Date().toLocaleTimeString()}...</h2>
+      </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      return (
+        <div className="App">
+            {setInterval(tick,1000)}
+        </div>
+      );
+
 }
 
 export default App;
