bd7579356868bbf397f98b560e4ab1533f4fd040
VartanovMaks <vartanov2009@gmail.com>
2021-03-23 11:27:11 +0200

    Clock via class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-function tick(){
-  const element = (
-      <div>
-        <h1>Hello, world</h1>
-        <h2> It is {new Date().toLocaleTimeString()}...</h2>
-      </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    componentDidMount() {
+        this.timedId = setInterval(
+            ()=>this.tick(),
+            1000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState({date: new Date()});
+    }
+     render() {
+        return (
+            <div>
+                <h1>Привет, мир!</h1>
+                <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 function App() {
 
-      return (
+    return (
         <div className="App">
-            {setInterval(tick,1000)}
+           <Clock />
         </div>
-      );
+    );
 
 }
 
