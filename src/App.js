b4dc492376ac5169c28b0b2cf71934ac76f32878
VartanovMaks <vartanov2009@gmail.com>
2021-03-24 13:19:50 +0200

    Список без ключей

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import './App.css';
 import React from 'react';
 
-function UserGreeting(props) {
-    return <h1>С возвращением!</h1>;
-}
-
-function GuestGreeting(props) {
-    return <h1>Войдите, пожалуйста.</h1>;
-}
-
-function LoginButton(props) {
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>{number}</li>
+    );
     return (
-        <button onClick={props.onClick}>
-            Войти
-        </button>
+        <ul>{listItems}</ul>
     );
 }
 
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Выйти
-        </button>
-    );
-}
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting />;
-        }
-    return <GuestGreeting />;
-}
-
-
-class LoginControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
-    }
-
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
-    }
-
-    render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-        if (isLoggedIn) {
-            button = <LogoutButton onClick={this.handleLogoutClick} />;
-        } else {
-            button = <LoginButton onClick={this.handleLoginClick} />;
-        }
-        return (
-            <div>
-                <Greeting isLoggedIn={isLoggedIn} />        {button}      </div>
-        );
-    }
-}
-
+const numbers = [1, 2, 3, 4, 5];
 function App(){
-    return <LoginControl />
+    return <NumberList numbers={numbers} />
 }
 
-// ReactDOM.render(
-//     <LoginControl />,
-//     document.getElementById('root')
-// );
 
 export default App;
