be9d6bb53bf32d304f50aa600187d6a5b61c3341
VartanovMaks <vartanov2009@gmail.com>
2021-03-24 14:14:53 +0200

    формы

--- src/App.js
+++ src/App.js
@@ -1,19 +1,91 @@
 import './App.css';
 import React from 'react';
 
-function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-        <li key={number.toString()}>{number}</li>
-    );
-    return (
-        <ul>{listItems}</ul>
-    );
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value    });
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Пойдут:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Количество гостей:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+}
+class FlavorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'coconut'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {    this.setState({value: event.target.value});  }
+    handleSubmit(event) {
+        alert('Ваш любимый вкус: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Выберите ваш любимый вкус:
+                    <select value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Грейпфрут</option>
+                        <option value="lime">Лайм</option>
+                        <option value="coconut">Кокос</option>
+                        <option value="mango">Манго</option>
+                    </select>
+                </label>
+                <input type="submit" value="Отправить" />
+            </form>
+        );
+    }
 }
 
-const numbers = [1, 2, 3, 4, 5];
 function App(){
-    return <NumberList numbers={numbers} />
+    return(
+            <div className="App">
+                <Reservation/>
+
+                <FlavorForm/>
+            </div>
+        )
+
+
+
 }
 
 
